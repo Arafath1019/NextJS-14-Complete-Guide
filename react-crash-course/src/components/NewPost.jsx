a3afa186429111f4c2a6aa7e5cf8c94833e7c0bc
yeasin.arafath <yeasin.arafath@bjitgroup.com>
2024-03-07 11:26:50 +0600

    feat: add usestate in NewPost component

--- react-crash-course/src/components/NewPost.jsx
+++ react-crash-course/src/components/NewPost.jsx
@@ -1,12 +1,18 @@
 import classes from './NewPost.module.css';
+import {useState} from 'react';
 
 function NewPost() {
+    const [enteredBody, setEnteredBody] = useState('');
+    const changeBodyHandler = (event) => {
+        setEnteredBody(event.target.value);
+    }
   return (
     <form className={classes.form}>
         <p>
             <label htmlFor="body">Text</label>
-            <textarea id="body" required rows={3} />
+            <textarea id="body" required rows={3} onChange={changeBodyHandler} />
         </p>
+        <p>{enteredBody}</p>
         <p>
             <label htmlFor="name">Your Name</label>
             <input id="name" type="text" required />
