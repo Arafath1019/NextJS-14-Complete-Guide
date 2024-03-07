63c6f6ea338524f3c05eb0ea79b83479ca94a67b
yeasin.arafath <yeasin.arafath@bjitgroup.com>
2024-03-07 11:18:11 +0600

    feat: create NewPost component

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
-import classes from './PostsList.module.css';
+import classes from "./PostsList.module.css";
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximillian" body="Reactjs" />
-      <Post author="Manuel" body="Check out" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Maximillian" body="Reactjs" />
+        <Post author="Manuel" body="Check out" />
+      </ul>
+    </>
   );
 }
 
