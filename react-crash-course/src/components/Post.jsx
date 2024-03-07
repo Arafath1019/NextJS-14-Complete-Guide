1c845228b182de207cc399222bd6f03cbc1a3589
yeasin.arafath <yeasin.arafath@bjitgroup.com>
2024-03-07 10:35:09 +0600

    feat: css styling & css modules

--- react-crash-course/src/components/Post.jsx
+++ react-crash-course/src/components/Post.jsx
@@ -1,9 +1,11 @@
+import classes from './Post.module.css';
+
 function Post(props) {
   return (
-    <>
-      <div>{props.author}</div>
-      <div>{props.body}</div>
-    </>
+    <div className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
+    </div>
   );
 }
 
