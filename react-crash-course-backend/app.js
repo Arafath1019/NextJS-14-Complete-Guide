8ad40ac080ecc3aed1790de3c86e285602ba902e
yeasin.arafath <yeasin.arafath@bjitgroup.com>
2024-03-08 09:51:15 +0600

    feat: sending http post request

--- react-crash-course-backend/app.js
+++ react-crash-course-backend/app.js
@@ -1,34 +1,37 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-// const { getStoredPosts, storePosts} = require('./data/posts');
+let allPosts = [];
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    // Attach CORS headers
-    // Required when using a detached backend (that runs on a different domain)
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+  // Attach CORS headers
+  // Required when using a detached backend (that runs on a different domain)
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
 });
 
-app.get('/posts', async (req, res) => {
+app.get("/posts", async (req, res) => {});
 
-})
+app.get("/posts/:id", (req, res) => {});
 
-app.get('/posts/:id', (req, res) => {
-
-})
-
-app.post('/posts', (req, res) => {
-
-})
-app.get('/', (req, res) => {
-    return res.json({"message": "Server Connected"})
-})
+app.post("/posts", (req, res) => {
+  const postData = req.body;
+  const newPost = {
+    ...postData,
+    id: Math.random().toString(),
+  };
+  allPosts = [newPost, ...allPosts];
+  console.log(allPosts)
+  res.status(201).json({ message: "Stored new post", post: newPost });
+});
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Home route" });
+});
 
 app.listen(8080);
--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -2,33 +2,42 @@ import Modal from "./Modal";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
-import {useState} from 'react';
+import { useState } from "react";
 
 function PostsList({ isPosting, onStopPosting }) {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-    const addPostHandler = (postData) => {
-        setPosts((existingPosts) => [postData, ...existingPosts]);
-    }
+  const addPostHandler = (postData) => {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    setPosts((existingPosts) => [postData, ...existingPosts]);
+  };
 
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onCancel={onStopPosting}
-            onAddPost={addPostHandler}
-          />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 &&   <ul className={classes.posts}>
-        {posts.map((post, index) => <Post key={index}  author={post.author} body={post.body} />)}
-      </ul>}
-      {posts.length == 0 && <div style={{ textAlign: 'center', color: 'white'}}>
-        <h1>There are no posts yet!</h1>
-        <p>Start adding some!</p>
-        </div>}
-    
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post, index) => (
+            <Post key={index} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {posts.length == 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h1>There are no posts yet!</h1>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
