0fd2319ae0f22e1c24f7843c4c96b667e19ffa7e
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2024-03-21 20:50:45 +0600

    feat: extracting dynamic path segment data in nextjs page router

--- nextjs-page-router/pages/portfolio/[projectid].js
+++ nextjs-page-router/pages/portfolio/[projectid].js
@@ -1,4 +1,12 @@
+import { useRouter } from 'next/router';
+
 function PortfolioProjectPage() {
+    const router = useRouter();
+
+    console.log(router.query.projectid);
+
+    // send a request to some backend server
+    // to fetch the piece of data with an id of router.query.projectid
   return (
     <div>
         <h1>The Portfolio Project Page</h1>
