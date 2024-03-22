1cbb297cdafd7d6213a5c073374be561fca19c4d
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2024-03-23 01:09:50 +0600

    feat: navigating to dynamic routes

--- nextjs-page-router/pages/clients/index.js
+++ nextjs-page-router/pages/clients/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 function Clients() {
+  const clients = [
+    { id: "1", name: "First Client" },
+    { id: "2", name: "Second Client" },
+    { id: "3", name: "Third Client" },
+  ];
   return (
-    <div>Clients</div>
-  )
+    <div>
+      <h1>Clients</h1>
+      <ul>
+        {clients.map((client, index) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Clients;
