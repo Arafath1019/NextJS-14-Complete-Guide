d15acc652061feb7f340e745031b0306875dddb1
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2024-03-25 23:56:07 +0600

    feat: navigating programmatically

--- nextjs-page-router/pages/clients/[id]/index.js
+++ nextjs-page-router/pages/clients/[id]/index.js
@@ -1,6 +1,15 @@
+import {useRouter} from 'next/router';
+
 function SingleClient() {
+  const router = useRouter();
+  function loadProjectHandler(){
+    router.push('/clients/user/project-a');
+  }
   return (
+    <>
     <div>SingleClient</div>
+    <button onClick={loadProjectHandler}>Load Project A</button>
+    </>
   )
 }
 
